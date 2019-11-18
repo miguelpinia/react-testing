fd094f604a2d7c86eabc80bcf946b7319fc9fb83
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2019-11-18 13:26:27 -0600

    Agregando eventos a react.js.

---
+++ src/components/counter.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ['tag1', 'tag2', 'tag3']
+    /* imageUrl: 'https://picsum.photos/200' */
+
+  };
+  styles = {
+    fontSize: 50,
+    fontWeight: 'bold',
+  };
+
+  handleIncrement() {
+    console.log('Increment clicked', this);
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+      </div>
+    );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += (this.state.count === 0) ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const {count} = this.state;
+    const x = <h1>Zero</h1>;
+    return count === 0 ? 'Zero' : count;
+  }
+}
+
+export default Counter;
