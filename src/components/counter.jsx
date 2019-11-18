dc32f59289bf3857a4dd619d132f25216fce4557
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2019-11-18 14:01:01 -0600

    Pasando parámetros a los componentes.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     tags: ['tag1', 'tag2', 'tag3']
     /* imageUrl: 'https://picsum.photos/200' */
 
@@ -20,7 +20,7 @@ class Counter extends Component {
 
   handleIncrement = (product) => {
     console.log(product);
-    this.setState({count: this.state.count + 1});
+    this.setState({value: this.state.value + 1});
   };
 
   doHandleIncrement = () => {
@@ -28,22 +28,23 @@ class Counter extends Component {
   };
 
   render() {
+    console.log('props', this.props);
     return (
       <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatValue()}</span>
         <button onClick={() => {this.handleIncrement({id: 1})}} className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += (this.state.count === 0) ? "warning" : "primary";
+    classes += (this.state.value === 0) ? "warning" : "primary";
     return classes;
   }
 
-  formatCount() {
-    const {count} = this.state;
-    return count === 0 ? 'Zero' : count;
+  formatValue() {
+    const {value} = this.state;
+    return value === 0 ? 'Zero' : value;
   }
 }
 
